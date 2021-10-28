3d415c5def1f89aeb43781d3915f5cd14bd15814
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-10-28 16:02:42 +0200

    es.6 Done

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,15 +1,34 @@
 /*
-1 - L’utente inserisce due numeri in successione, con due prompt.
-Il software stampa il maggiore.
+6 - Genera un array di 50 numeri random. Nell’array non devono esserci doppioni.
 */
 
-let x = parseInt(prompt('Inserisci il primo numero x: '));
-let y = parseInt(prompt('Inserisci il secondo numero y: '));
+// impostiamo la ricerca per esempio su 100 numeri random in una mariabile che possiamo modificare sempre;
+//fincchè il contatore non arriva a 50 numeri tutti diversi
+// il pc genera sempre numeri random
 
-if (x>y){
-    console.log('Il numero maggiore è x; ',x);
-} else if (x<y){
-    console.log('Il numero maggiore è y; ',y);
-} else {
-    console.log('I numeri sono uguali');
+let arrayNum = [];
+let contatore = 0;
+const maxNumeri = 100;
+
+//finchè il contatore è minore di 50 numeri restiamo del ciclo
+while (contatore < 50){
+    //impostiamo una variabile con stato false e generiamo un numero casuale
+    let numTrovato = false;
+    let numRandom = Math.floor(Math.random() * maxNumeri);
+    
+    //cicliamo tutto l'arrary se il numero risulta già presente la variabile di stato diventa true
+    for ( i=0; i< arrayNum.length; i++){
+        if ( numRandom === arrayNum [i]){
+            numTrovato = true;
+        }
+    }
+
+    //se la variabile non è true aggiungiamo il numero appena generato nell'array ed incrementiamo il contatore iniziale
+    if (!numTrovato) {
+        arrayNum.push(numRandom);
+        contatore++
+    }
 }
+
+// stampiamo l'array per verificarlo.
+console.log(arrayNum);
